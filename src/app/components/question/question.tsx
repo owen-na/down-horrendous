664fef99c8b88ee76e68cb698862b704393454a2
owen <osn53387@uga.edu>
2023-12-17 21:25:24 -0500

    began on question component, refer to figma

---
+++ src/app/components/question/question.tsx
@@ -0,0 +1,11 @@
+import styles from "./question.module.css"
+
+export default function Question() {
+    return (
+        <div className={styles.mainbox}>
+            {/* <Image>
+
+            </Image> */}
+        </div>
+    )
+}
